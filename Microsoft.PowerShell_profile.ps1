f52ac591f4437513caf62688619169490f950c1c
dell <bryndelltorio@gmail.com>
2021-08-12 19:36:38 +0800

    automatic project sub-folders setup by invoking buildprj -Name <Project Name>

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,12 @@
 
-Import-Module 'C:\tools\poshgit\dahlbyk-posh-git-9bda399\src\posh-git.psd1'
+
+#Import-Module 'C:\tools\poshgit\dahlbyk-posh-git-9bda399\src\posh-git.psd1'
+
+Add-PoshGitToProfile
+
+#variable defined for quick folder switch.
+$prjdir = "$HOME\Documents\Design\Projects\"
+$prof = "$HOME\Documents\PowerShell\"
 
 # First system config for windows 10.
 function toff { shutdown /p }
@@ -15,6 +22,30 @@ function cdMovies {
   }
 Set-Alias -name movies -value cdMovies
 
+function buildprj_Folder_Heirarchy {
+  [CmdletBinding(SupportsShouldProcess)]
+  param (
+    [string]$Name
+  )
+New-item -Name $Name -Path . -ItemType Directory;
+Set-Location $Name; 
+New-Item -Name "Docs" -ItemType Directory;
+Set-Location .\Docs;
+New-Item -Name "README.txt" -ItemType File -Value "Place here all the documents related to project $Name.";
+Set-Location .\..;
+New-Item -Name "Datasheet" -ItemType Directory;
+Set-Location .\Datasheet;
+New-Item -Name "README.txt" -ItemType File -Value "Place here all the datasheet related to project $Name.";
+Set-Location .\..;
+New-Item -Name "forUpload" -ItemType Directory;
+Set-Location .\forUpload;
+New-Item -Name "README.txt" -ItemType File -Value "Place here project $Name schematics for release and sharepoint upload.";
+Set-Location .\..;
+Set-Location .\..;
+Write-Host "Done generating [$Name project sub-folders]" -ForegroundColor Green
+}
+Set-Alias -Name buildPrj -Value buildprj_Folder_Heirarchy
+
 function Open-Application {
 
   [CmdletBinding(SupportsShouldProcess)]
@@ -27,16 +58,22 @@ function Open-Application {
       Start-Process 'C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe'
     } elseif ($Name -eq "qbit") {
       Start-Process 'C:\Program Files\qBittorrent\qbittorrent.exe'
-    } elseif ($Name -eq "setal") {
-      Start-Process 'C:\Users\GAIA\Documents\PowerShell\Microsoft.PowerShell_profile'
+    } elseif ($Name -eq "prof") {
+      Start-Process "C:\Users\GAIA\AppData\Local\Programs\Microsoft VS Code Insiders\bin\Code-Insiders" 'C:\Users\GAIA\Documents\PowerShell\Microsoft.PowerShell_profile.ps1'
     } elseif ($Name -eq "prj1") {
-      Start-Process 'C:\Users\GAIA\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Cadence Release 17.2-2016\OrCAD Products\Capture CIS' 'C:\Users\GAIA\Desktop\Design\Projects\DSSTI02_Control\DSSTI02_CONTROL.opj'
+      Start-Process 'C:\Users\GAIA\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Cadence Release 17.2-2016\OrCAD Products\Capture CIS' 'C:\Users\GAIA\Documents\Design\Projects\DSSTI02_Control\DSSTI02_CONTROL.opj'
     } elseif ($Name -eq "prj2") {
-      Start-Process 'C:\Users\GAIA\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Cadence Release 17.2-2016\OrCAD Products\Capture CIS' 'C:\Users\GAIA\Desktop\Design\Projects\DSSTI02_Sentinel\DSSTI02_SENTINEL.opj'
+      Start-Process 'C:\Users\GAIA\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Cadence Release 17.2-2016\OrCAD Products\Capture CIS' 'C:\Users\GAIA\Documents\Design\Projects\DSSTI02_Sentinel\DSSTI02_SENTINEL.opj'
     } elseif ($Name -eq "snptl") {
       Start-Process "snippingtool"
+    } elseif ($Name -eq "pcb") {
+      Start-Process "C:\Cadence\SPB_17.2\tools\bin\allegro.exe"
+    } elseif ($Name -eq "sch") {
+      Start-Process "C:\Cadence\SPB_17.2\tools\bin\capture.exe"
+    } elseif ($Name -eq "pad") {
+      Start-Process "C:\Cadence\SPB_17.2\tools\bin\padstack_editor.exe"
     } elseif ($Name -eq "bom") {
-      Start-Process 'C:\Users\GAIA\Desktop\Design\Cadence Configuration reference\BOM processing tool.xlsm'
+      Start-Process 'C:\Users\GAIA\Documents\Design\Cadence Configuration reference\BOM processing tool.xlsm'
     } elseif ($Name -eq "word") {
       Start-Process 'C:\Program Files (x86)\Microsoft Office\root\Office16\winword.exe' -WindowStyle Maximized
       Clear-Host
@@ -51,10 +88,12 @@ function Open-Application {
       Write-Host "[executing Microsoft PowerPoint]" -ForegroundColor Green
     } elseif ($Name -eq "datasheet") {
       Start-Process 'C:\Users\GAIA\OneDrive - Integrated Micro-Electronics Inc\Datasheet'
+    } elseif ($Name -eq "review") {
+      Start-Process 'C:\Users\GAIA\OneDrive - Integrated Micro-Electronics Inc\Design\Projects\Review'
     } elseif ($Name -eq "prjdir") {
-      Start-Process 'C:\Users\GAIA\Desktop\Design\Projects'
+      Start-Process 'C:\Users\GAIA\Documents\Design\Projects'
     } elseif ($Name -eq "ref") {
-      Start-Process 'C:\Users\GAIA\Desktop\Design\Projects\reference'
+      Start-Process 'C:\Users\GAIA\Documents\Design\Projects\reference'
     } else {
       Write-Host "[$Name not found.]" -ForegroundColor Red
     }
@@ -66,7 +105,7 @@ function Close-Application {
 
   [CmdletBinding(SupportsShouldProcess)]
   param ( 
-    [string]$ApplicationName
+    [string]$Name
   )
 
   process {
@@ -87,29 +126,29 @@ function Close-Application {
     $_staDatasheet2 = Get-Process -Name "explorer" -ErrorAction SilentlyContinue
     $_staMovies = Get-Process -Name "explorer" -ErrorAction SilentlyContinue
     $_staMsTeams = Get-Process -Name "teams" -ErrorAction SilentlyContinue
-    Write-Host "[closing $ApplicationName]" -ForegroundColor Red
+    Write-Host "[closing $Name]" -ForegroundColor Red
 
-    if (($_openQbit.HasExited -eq $false) -and ($ApplicationName -eq "qbit")) {
+    if (($_openQbit.HasExited -eq $false) -and ($Name -eq "qbit")) {
       Stop-Process -Name "qbittorrent"
-    } elseif (($_setAlias.HasExited -eq $false) -and ($ApplicationName -eq "code")) {
+    } elseif (($_setAlias.HasExited -eq $false) -and ($Name -eq "code")) {
       Stop-Process -Name "Code - Insiders"
-    } elseif (($_open_Edge.HasExited -eq $false) -and ($ApplicationName -eq "edge")) {
+    } elseif (($_open_Edge.HasExited -eq $false) -and ($Name -eq "edge")) {
       Stop-Process -InputObject $_open_Edge
-    } elseif (($_openWinWord.HasExited -eq $false) -and ($ApplicationName -eq "word")) {
+    } elseif (($_openWinWord.HasExited -eq $false) -and ($Name -eq "word")) {
       Stop-Process -InputObject $_openWinWord
-    } elseif (($_sniptool.HasExited -eq $false) -and ($ApplicationName -eq "edge")) {
+    } elseif (($_sniptool.HasExited -eq $false) -and ($Name -eq "edge")) {
       Stop-Process -Name "snippingtool"
-    } elseif ((($_openExcel.HasExited -eq $false) -or ($_staBomTool.HasExited -eq $false)) -and ($ApplicationName -eq "excel")) {
+    } elseif ((($_openExcel.HasExited -eq $false) -or ($_staBomTool.HasExited -eq $false)) -and ($Name -eq "excel")) {
       Stop-Process -Name "EXCEL"
-    } elseif (($_openPowerPnt.HasExited -eq $false) -and ($ApplicationName -eq "powerpnt")) {
+    } elseif (($_openPowerPnt.HasExited -eq $false) -and ($Name -eq "powerpnt")) {
       Stop-Process -InputObject $_openPowerPnt
-    } elseif ((($_staPrj1.HasExited -eq $false) -or ($_staPrj2.HasExited -eq $false)) -and ($ApplicationName -eq "Capture")) {
+    } elseif ((($_staPrj1.HasExited -eq $false) -or ($_staPrj2.HasExited -eq $false)) -and ($Name -eq "Capture")) {
       Stop-Process -Name "Capture"
-    } elseif ((($_staReference.HasExited -eq $false) -or ($_staDatasheet.HasExited -eq $false) -or ($_staDatasheet1.HasExited -eq $false) -or ($_staDatasheet2.HasExited -eq $false) -or ($_staMovies.HasExited -eq $false) -or ($_prjdirectory.HasExited -eq $false)) -and ($ApplicationName -eq "explorer")) {
+    } elseif ((($_staReference.HasExited -eq $false) -or ($_staDatasheet.HasExited -eq $false) -or ($_staDatasheet1.HasExited -eq $false) -or ($_staDatasheet2.HasExited -eq $false) -or ($_staMovies.HasExited -eq $false) -or ($_prjdirectory.HasExited -eq $false)) -and ($Name -eq "explorer")) {
       Stop-Process -Name "explorer"
-    } elseif (($_staMsTeams.HasExited -eq $false) -and ($ApplicationName -eq "teams")) {
+    } elseif (($_staMsTeams.HasExited -eq $false) -and ($Name -eq "teams")) {
       Stop-Process -InputObject $_staMsTeams
-    } elseif ($ApplicationName -eq "all") {
+    } elseif ($Name -eq "all") {
       Stop-Process -Name "Capture" -ErrorAction SilentlyContinue
       Stop-Process -Name "Code - Insiders" -ErrorAction SilentlyContinue
       Stop-Process -Name "qbittorrent" -ErrorAction SilentlyContinue
@@ -143,4 +182,4 @@ if (Test-Path($ChocolateyProfile)) {
             https://www.youtube.com/watch?v=n2-wZDux8L4
 #>
 
-. 'C:\tools\poshgit\dahlbyk-posh-git-9bda399\install.ps1'
+#. 'C:\tools\poshgit\dahlbyk-posh-git-9bda399\install.ps1'
